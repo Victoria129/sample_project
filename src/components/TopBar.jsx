973e02c7d9a94a44c32fe460b7f0c4e719fa04c4
Luzinda Douglas <89447046+LDouglasOT@users.noreply.github.com>
2021-10-22 14:59:32 +0300

    all

---
+++ src/components/TopBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+import { theme } from '../Module/Theme'
+
+const Button=styled.div`
+background-Color:theme.colors.bgbutton;
+align-Items:center;
+`
+
+
+function TopBar() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default TopBar
